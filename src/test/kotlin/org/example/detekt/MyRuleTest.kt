04045da1ec8f605a7dfc7bb6f83dfb1133214b74
Brais Gabín <braisgabin@gmail.com>
2021-12-01 21:20:40 +0100

    Enable tests with Type Solving

--- src/test/kotlin/org/example/detekt/MyRuleTest.kt
+++ src/test/kotlin/org/example/detekt/MyRuleTest.kt
@@ -1,8 +1,13 @@
 package org.example.detekt
 
 import com.google.common.truth.Truth.assertThat
+import io.github.detekt.test.utils.KotlinCoreEnvironmentWrapper
+import io.github.detekt.test.utils.createEnvironment
 import io.gitlab.arturbosch.detekt.api.Config
-import io.gitlab.arturbosch.detekt.test.compileAndLint
+import io.gitlab.arturbosch.detekt.test.compileAndLintWithContext
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 
 internal class CustomRuleSpec {
@@ -14,7 +19,7 @@ internal class CustomRuleSpec {
           inner class B
         }
         """
-        val findings = MyRule(Config.empty).compileAndLint(code)
+        val findings = MyRule(Config.empty).compileAndLintWithContext(env, code)
         assertThat(findings).hasSize(1)
     }
 
@@ -25,7 +30,21 @@ internal class CustomRuleSpec {
           class B
         }
         """
-        val findings = MyRule(Config.empty).compileAndLint(code)
+        val findings = MyRule(Config.empty).compileAndLintWithContext(env, code)
         assertThat(findings).isEmpty()
+    }
+
+    private lateinit var envWrapper: KotlinCoreEnvironmentWrapper
+    private val env: KotlinCoreEnvironment
+        get() = envWrapper.env
+
+    @Before
+    fun setUp() {
+        envWrapper = createEnvironment()
+    }
+
+    @After
+    fun tearDown() {
+        envWrapper.dispose()
     }
 }
